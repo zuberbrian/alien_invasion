77a7c9e8e51020e12955e2436ea7a965b5acc085
zuberbrian <37417090+zuberbrian@users.noreply.github.com>
2019-01-18 19:24:12 -0800

    Add files via upload

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Group
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 
@@ -10,16 +11,19 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    stats = GameStats(ai_settings)
     # Make ship, bullets, and aliens
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
     # Create many aliens
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)        
 run_game()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,8 +4,12 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230,230,230)
         self.ship_speed_factor = 1.5
-        self.bullet_speed_factor = 1
+        self.ship_limit = 3
+        self.bullet_speed_factor = 2
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 5
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
