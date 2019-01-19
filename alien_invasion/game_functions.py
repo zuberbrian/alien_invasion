8a5f1e53cd78dc9d550a64e146610e8cecf76e07
zuberbrian <37417090+zuberbrian@users.noreply.github.com>
2019-01-18 23:13:18 -0800

    Add files via upload

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -4,7 +4,7 @@ import pygame
 from bullet import Bullet
 from alien import Alien
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -12,6 +12,21 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
+
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
+        ai_settings.initialize_dynamic_settings()
+        pygame.mouse.set_visible(False)
+        stats.reset_stats()
+        stats.game_active = True
+        aliens.empty()
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
         if event.key == pygame.K_RIGHT:
@@ -29,27 +44,34 @@ def check_keyup_events(event, ship):
         elif event.key == pygame.K_LEFT: 
             ship.moving_left = False
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
         screen.fill(ai_settings.bg_color)
         for bullet in bullets.sprites():
             bullet.draw_bullet()
         ship.blitme()
-        aliens.draw(screen)       
+        aliens.draw(screen)
+        sb.show_score()
+        if not stats.game_active:
+            play_button.draw_button()      
         # Make screen visible
         pygame.display.flip()
 
-def update_bullets(ai_settings, screen, ship, aliens, bullets):
+def update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets):
     bullets.update()
     # Get rid of bullets once off screen
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+    check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    if collisions:
+        stats.score += ai_settings.alien_points
+        sb.prep_score()
     if len(aliens) == 0:
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
@@ -106,6 +128,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
             sleep(0.5)
         else:
             stats.game_active = False
+            pygame.mouse.set_visible(True)
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
     screen_rect = screen.get_rect()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,15 +1,30 @@
 class Settings():
     def __init__(self):
+        # Screen
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
-        self.ship_speed_factor = 1.5
+        # Ship
         self.ship_limit = 3
-        self.bullet_speed_factor = 2
+        # Bullet
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 5
-        self.alien_speed_factor = 1
+        # Aliens
         self.fleet_drop_speed = 10
+        # Speed
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+    
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
         self.fleet_direction = 1
+        self.alien_points = 50
+    
+    def increase_speed(self):
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
